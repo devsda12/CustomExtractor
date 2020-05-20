22e1718e83fc7444f03a5d16e302c0ef2440f506
Jasper <jmbalster@hotmail.com>
2020-05-20 08:45:23 +0200

    Custom class created for the WizPages

--- CustomExtractor/MainWindow.xaml.cs
+++ CustomExtractor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CustomExtractor.Models;
 
 namespace CustomExtractor
 {
@@ -20,36 +21,38 @@ namespace CustomExtractor
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Page> pages = new List<Page>();
-        int lastAddedPageIndex = 0;
+        List<WizPage> pages = new List<WizPage>();
+        int lastAddedPageIndex, currentPageIndex = 0;
         public MainWindow()
         {
             InitializeComponent();
 
             //Initializing the pages
-            pages.Add(new WizPages.BrowsePage());
-            pages.Add(new WizPages.ProgressPage());
+            pages.Add(new WizPage(new WizPages.BrowsePage()));
+            pages.Add(new WizPage(new WizPages.ProgressPage(), canGoBack: false, canGoForward: false));
+            pages.Add(new WizPage(new WizPages.ReturnPage(), canGoBack: false));
 
             //Setting the browse as the default appearing page in the contentframe
-            ContentFrame.Content = pages[0];
+            ContentFrame.Content = pages[0].page;
 
             //Disabling the back button
             Back_Button.IsEnabled = false;
         }
 
-        //Navigation bar button clicks under here
+        //Navigation bar button clicks and updates under here
         private void Back_Button_Click(object sender, RoutedEventArgs e)
         {
-            if (ContentFrame.CanGoBack)
+            if (ContentFrame.CanGoBack && pages[currentPageIndex].canGoBack)
             {
                 ContentFrame.GoBack();
 
-                //Checking if the back button should be disabled
-                if (!ContentFrame.CanGoBack) Back_Button.IsEnabled = false;
+                //Modding the index
+                currentPageIndex--;
+                Button_Update(pages[currentPageIndex]);
             }
         }
 
-        private void Extract_Button_Click(object sender, RoutedEventArgs e)
+        private void Next_Button_Click(object sender, RoutedEventArgs e)
         {
             if (ContentFrame.CanGoForward)
             {
@@ -60,16 +63,44 @@ namespace CustomExtractor
                 if (lastAddedPageIndex < pages.Count - 1)
                 {
                     lastAddedPageIndex++;
-                    ContentFrame.Navigate(pages[lastAddedPageIndex]);
+                    ContentFrame.Navigate(pages[lastAddedPageIndex].page);
+                } else if(currentPageIndex == pages.Count - 1)
+                {
+                    Close();
+                    return;
                 }
             }
 
-            if (!Back_Button.IsEnabled) Back_Button.IsEnabled = true;
+            //Checking if back button should be enabled after updating current index
+            currentPageIndex++;
+            Button_Update(pages[currentPageIndex]);
+            //if (Back_Button.IsEnabled != pages[currentPageIndex].canGoBack) Back_Button.IsEnabled = !Back_Button.IsEnabled; //Quite cool, always works as it checks if enabled is equal to if it may be enabled and the other way around
         }
 
         private void Cancel_Button_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Button_Update(WizPage newPage)
+        {
+            //First default checks, is it the first or last index. If so, this overrules the WizPage parameters
+            if (currentPageIndex == 0) Back_Button.IsEnabled = false;
+            if (currentPageIndex == pages.Count - 1) Next_Button.Content = "Finish";
+            else Next_Button.Content = "Next";
+
+            //Now checking the newPage properties
+            if (newPage.canGoBack)
+            {
+                Back_Button.IsEnabled = true;
+            }
+            else Back_Button.IsEnabled = false;
+
+            if (newPage.canGoForward)
+            {
+                Next_Button.IsEnabled = true;
+            }
+            else Next_Button.IsEnabled = false;
         }
     }
 }
---
+++ CustomExtractor/Models/WizPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace CustomExtractor.Models
+{
+    /// <summary>
+    /// Wrapper for a default Page which also contains some own set properties
+    /// </summary>
+    public class WizPage
+    {
+        //Custom variables under here
+        public Page page;
+        public bool canGoBack, canGoForward = true;
+
+        //Constructor
+        public WizPage(Page page, bool canGoBack = true, bool canGoForward = true)
+        {
+            this.page = page;
+            this.canGoBack = canGoBack;
+            this.canGoForward = canGoForward;
+        }
+    }
+}
